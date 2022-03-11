0b6694a8ea851803aab7c373d6bf7fdc87104b9f
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2022-03-12 00:28:31 +0700

    Added a build script for bash, and improved the bulld script for Windows Fix a bit of problem with the table in the URLs tab Minor improvement

--- build.bat
+++ build.bat
@@ -1,1 +1,24 @@
-mkdir build || del /s / q build\ && npx sass src:build --style compressed --source-map && npx tsc --project ./tsconfig.json && xcopy src\*.html /s build && xcopy src\icons /s build\icons\ && copy src\*.* build
+@echo off
+
+echo ===^> Start building...
+
+echo ===^> Resetting build folder...
+mkdir build || del /s / q build\
+
+echo ===^> Copying files...
+xcopy "src\*" build\ /E
+
+echo ===^> Compiling stylesheets...
+call npx sass src:build --style compressed --source-map
+
+echo  --- Build error from this line is OK to ignore --- 
+echo ===^> Compiling scripts...
+call npx tsc --project ./tsconfig.json
+echo  --- Build error from this line is now NOT OK to ignore --- 
+
+echo ===^> Deleting unused files...
+del /s /q build\*.ts
+del /s /q build\*.scss
+del /s /q build\*.sass
+
+echo ===^> Done!
---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+commandExist() {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo "No"
+        #return 1;
+    else
+        echo "Yes"
+        #return 0;
+    fi
+}
+
+txtblk='\e[0;30m' # Black - Regular
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtcyn='\e[0;36m' # Cyan
+txtwht='\e[0;37m' # White
+
+isNpxExist=$(commandExist npx)
+
+if [ $isNpxExist = "No" ]; then
+    echo "Error: Please install npx"
+    exit 1;
+fi
+echo -e "${txtgrn}===>${txtwht} Start building..."
+
+echo -e "${txtgrn}===>${txtwht} Resetting build folder..."
+mkdir build
+rm -rfv build/*
+
+echo -e "${txtgrn}===>${txtwht} Copying files..."
+cp -R src/. build/
+
+echo -e "${txtgrn}===>${txtwht} Compiling stylesheets..."
+npx sass build --style compressed --source-map
+
+echo -e "${txtblu} --- Build error from this line is OK to ignore --- ${txtwht}"
+echo -e "${txtgrn}===>${txtwht} Compiling scripts..."
+npx tsc --project ./tsconfig.json
+echo -e "${txtred} --- Build error from this line is now NOT OK to ignore --- ${txtwht}"
+
+echo -e "${txtgrn}===>${txtwht} Deleting unused files..."
+find build -type f \( -iname *.ts -o -iname *.sass -o -iname *.scss \) -exec rm -fv {} \;
+
+echo -e "${txtgrn}===>${txtwht} Done!"
